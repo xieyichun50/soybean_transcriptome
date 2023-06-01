5a03472c2589fd2dedd4f407f18c97010aaa6d0d
Yichun <59522685+xieyichun50@users.noreply.github.com>
2023-06-01 17:28:43 +0800

    Create expression.deg.sh

---
+++ scripts/expression.deg.sh
@@ -0,0 +1,9 @@
+wd=
+dir_fq=${wd}/fastq
+dir_bam=${wd}/alignment
+dir_exp=${wd}/expression
+refgenome=${wd}/ref_genome/Phytozome/PhytozomeV13/Gmax/Wm82.a4.v1/assembly/Gmax_508_v4.0.softmasked.fa
+gff=${wd}/ref_genome/Phytozome/PhytozomeV13/Gmax/Wm82.a4.v1/annotation/Gmax_508_Wm82.a4.v1.gene_exons.gff3
+
+/usr/bin/Rscript /mnt/content_176/yichun/scripts/nanopore/create_sample_pair.R -i samples_n_reads_described.txt
+/mnt/content_176/yichun/tools/trinityrnaseq/Analysis/DifferentialExpression/run_DE_analysis.pl --matrix gene_count_matrix.csv --method edgeR --samples_file ${wd}/samples_n_reads_described.txt --output edgeR_gene.min_reps2.min_cpm1 --min_reps_min_cpm 2,1 --contrasts ${wd}/sample_pairs.txt
